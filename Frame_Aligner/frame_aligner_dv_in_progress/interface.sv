ffbce4d38bce6cafbad5412ba55f19709b739a07
Roei341 <163875747+Roei341@users.noreply.github.com>
2025-10-29 20:00:50 +0200

    Delete Frame_Aligner/frame_aligner_dv_in_progress directory

--- Frame_Aligner/frame_aligner_dv_in_progress/interface.sv
+++
@@ -1,7 +0,0 @@
-interface inf (input logic clk, reset);
-   logic  [7:0] rx_data;
-   logic  [3:0]  fr_byte_position; 
-   logic frame_detect;
-  
-  modport DUT(input clk, rx_data, reset, output fr_byte_position, frame_detect);
-endinterface
